7b21b6932c0d9faf10043d64da75ec666827a43e
jcrandolph <jackcrandi@gmail.com>
2018-11-19 01:06:23 -0500

    commit3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,36 @@
+
+function compPick(){
 let alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(randomLetter)}
 
+let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(randomLetter)}
 let wins= 0;
 
 let losses= 0;
 
 let guessesLeft= 10;
 
-//document.onkeyup = function(event){
-    //let userGuess = event.key.toLowerCase();
-
-
-console.log("hello");
-
-
-let userGuess= document.getElementById("userGuess");
+let guesses= [];
+    //when user presses letters on keyboard run this function
 document.onkeyup = function(event){
-  userGuess.textContent = event.key;
+    let userGuess= event.key;
+   
+    if (randomLetter === userGuess){
+        //start new round
+        wins++; guessesLeft= 10; guesses= []; compPick();
 }
-  
-
-if (randomLetter === userGuess){
-        //reset game
-        wins++; guessesLeft= 10; userGuess= "";
-}
-else {
-   guessesLeft--; //userGuess appears with comma
+    else {
+        guessesLeft--; guesses.push(userGuess);
 }
 
-if (guessesLeft === 0){
-    losses++
+    if (guessesLeft === 0){
+        losses++; guessesLeft= 10;
 }
-
+ //html
+ document.getElementById("wins").innerHTML = " " + wins;
+ document.getElementById("losses").innerHTML = " " + losses;
+ document.getElementById("guessesLeft").innerHTML = " " + guessesLeft;
+ document.getElementById("userGuess").innerHTML = " " + guesses + " ";
+}
