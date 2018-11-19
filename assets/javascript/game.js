b3fb9e85bb64ea42cfa8508fda092eaf7fb11635
jcrandolph <jackcrandi@gmail.com>
2018-11-19 01:44:40 -0500

    commit4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
 
-function compPick(){
+//generate a random letter
 let alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(randomLetter)}
+        console.log(randomLetter); 
 
-let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(randomLetter)}
+//variables
 let wins= 0;
 
 let losses= 0;
@@ -13,20 +12,21 @@ let losses= 0;
 let guessesLeft= 10;
 
 let guesses= [];
+
     //when user presses letters on keyboard run this function
 document.onkeyup = function(event){
     let userGuess= event.key;
    
     if (randomLetter === userGuess){
         //start new round
-        wins++; guessesLeft= 10; guesses= []; compPick();
+        wins++; guessesLeft= 10; guesses= []; 
 }
     else {
         guessesLeft--; guesses.push(userGuess);
 }
 
     if (guessesLeft === 0){
-        losses++; guessesLeft= 10;
+        losses++; guessesLeft= 10; guesses= [];
 }
  //html
  document.getElementById("wins").innerHTML = " " + wins;
