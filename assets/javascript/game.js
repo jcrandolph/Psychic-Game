e2cdbae8b2649c003a0513e69789d376dd0e2eb7
jcrandolph <jackcrandi@gmail.com>
2018-11-19 23:18:35 -0500

    commit6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,47 @@
 
 //generate a random letter
-let alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 let randomLetter = generateRandLetter();
-      console.log(randomLetter); 
+console.log(randomLetter);
 
 function generateRandLetter() {
     return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
 //variables
-let wins= 0;
+let wins = 0;
 
-let losses= 0;
 
-let guessesLeft= 10;
+let losses = 0;
 
-let guesses= [];
+let guessesLeft = 10;
 
-    //when user presses letters on keyboard run this function
-document.onkeyup = function(event){
-    let userGuess= event.key;
-   
-    if (randomLetter === userGuess){
+let guesses = [];
+
+//when user presses letters on keyboard run this function
+document.onkeyup = function (event) {
+    let userGuess = event.key;
+
+    if (randomLetter === userGuess) {
         //start new round
-        wins++; guessesLeft= 10; guesses= [];
+        wins++;
+        guessesLeft = 10;
+        guesses = [];
         randomLetter = generateRandLetter();
-        console.log(randomLetter);  
-}
+        console.log(randomLetter);
+    }
     else {
         guessesLeft--; guesses.push(userGuess);
-}
+    }
 
-    if (guessesLeft === 0){
-        losses++; guessesLeft= 10; guesses= [];
+    if (guessesLeft === 0) {
+        losses++; guessesLeft = 10; guesses = [];
         randomLetter = generateRandLetter();
-}
- //html
- document.getElementById("wins").innerHTML = " " + wins;
- document.getElementById("losses").innerHTML = " " + losses;
- document.getElementById("guessesLeft").innerHTML = " " + guessesLeft;
- document.getElementById("userGuess").innerHTML = " " + guesses + " ";
+    }
+    //html
+    document.getElementById("wins").innerHTML = " " + wins;
+    document.getElementById("losses").innerHTML = " " + losses;
+    document.getElementById("guessesLeft").innerHTML = " " + guessesLeft;
+    document.getElementById("userGuess").innerHTML = " " + guesses + " ";
 }
